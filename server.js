5db47ac29d954519e3d9c2bf1b21432ade701cab
Wire Snark <wsnark@tuta.io>
2018-02-10 19:19:04 +0300

    HTTP-only service

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-//const http = require('http');
-const https = require('https');
+const http = require('http');
+//const https = require('https');
 const fs = require('fs');
 const express = require('express');
 
 const app = express();
-//const server = http.createServer(app);
-const httpsOptions = {
-    key: fs.readFileSync('./certs/key.pem'),
-    cert: fs.readFileSync('./certs/chain.pem'),
-    passphrase: "abc345"
-}
+const server = http.createServer(app);
+//const httpsOptions = {
+//    key: fs.readFileSync('./certs/key.pem'),
+//    cert: fs.readFileSync('./certs/chain.pem'),
+//    passphrase: "abc345"
+//}
 // as this is demo repo, passphrase exposed above ^^^ is for demo cert -- it is OK.
 
-const server = https.createServer(httpsOptions, app);
+//const server = https.createServer(httpsOptions, app);
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
